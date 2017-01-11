3940eada3000f0fc87991854c49279e41313d917
Marc Larkin and Shruti Priya <marc@>
2017-01-10 16:17:24 -0800

    HTML classes and scripts to make panels appear and disappear by sliding

---
+++ js/scripts.js
@@ -0,0 +1,168 @@
+var test = "it works";
+
+$(document).ready(function(){
+
+  $(".panel-heading.comments").click(function(){
+    $(".panel.comments").slideToggle();
+  });
+
+  $(".panel.comments").click(function(){
+    $("panel-heading.comments").toggle();
+
+  });
+
+  $(".panel-heading.js").click(function(){
+    $(".panel.js").slideToggle();
+  });
+
+  $(".panel.js").click(function(){
+    $("panel-heading.js").toggle();
+
+  });
+
+  $(".panel-heading.oper").click(function(){
+    $(".panel.oper").slideToggle();
+  });
+
+  $(".panel.oper").click(function(){
+    $("panel-heading.oper").toggle();
+
+  });
+  $(".panel-heading.var").click(function(){
+    $(".panel.var").slideToggle();
+  });
+
+  $(".panel.var").click(function(){
+    $("panel-heading.var").toggle();
+
+  });
+  $(".panel-heading.vnc").click(function(){
+    $(".panel.vnc").slideToggle();
+  });
+
+  $(".panel.vnc").click(function(){
+    $("panel-heading.vnc").toggle();
+
+  });
+  $(".panel-heading.func").click(function(){
+    $(".panel.func").slideToggle();
+  });
+
+  $(".panel.func").click(function(){
+    $("panel-heading.func").toggle();
+
+  });
+  $(".panel-heading.met").click(function(){
+    $(".panel.met").slideToggle();
+  });
+
+  $(".panel.met").click(function(){
+    $("panel-heading.met").toggle();
+
+  });
+  $(".panel-heading.arg").click(function(){
+    $(".panel.arg").slideToggle();
+  });
+
+  $(".panel.arg").click(function(){
+    $("panel-heading.arg").toggle();
+
+  });
+  $(".panel-heading.par").click(function(){
+    $(".panel.par").slideToggle();
+  });
+
+  $(".panel.par").click(function(){
+    $("panel-heading.par").toggle();
+
+  });
+  $(".panel-heading.ret").click(function(){
+    $(".panel.ret").slideToggle();
+  });
+
+  $(".panel.ret").click(function(){
+    $("panel-heading.ret").toggle();
+
+  });
+  $(".panel-heading.chain").click(function(){
+    $(".panel.chain").slideToggle();
+  });
+
+  $(".panel.chain").click(function(){
+    $("panel-heading.chain").toggle();
+
+  });
+  $(".panel-heading.boo").click(function(){
+    $(".panel.boo").slideToggle();
+  });
+
+  $(".panel.boo").click(function(){
+    $("panel-heading.boo").toggle();
+
+  });
+  $(".panel-heading.undef").click(function(){
+    $(".panel.undef").slideToggle();
+  });
+
+  $(".panel.undef").click(function(){
+    $("panel-heading.undef").toggle();
+
+  });
+  $(".panel-heading.NaN").click(function(){
+    $(".panel.NaN").slideToggle();
+  });
+
+  $(".panel.NaN").click(function(){
+    $("panel-heading.NaN").toggle();
+
+  });
+  $(".panel-heading.escape").click(function(){
+    $(".panel.escape").slideToggle();
+  });
+
+  $(".panel.escape").click(function(){
+    $("panel-heading.escape").toggle();
+
+  });
+  $(".panel-heading.alert").click(function(){
+    $(".panel.alert").slideToggle();
+  });
+
+  $(".panel.alert").click(function(){
+    $("panel-heading.alert").toggle();
+
+  });
+  $(".panel-heading.jQuery").click(function(){
+    $(".panel.jQuery").slideToggle();
+  });
+
+  $(".panel.jQuery").click(function(){
+    $("panel-heading.jQuery").toggle();
+
+  });
+  $(".panel-heading.att").click(function(){
+    $(".panel.att").slideToggle();
+  });
+
+  $(".panel.att").click(function(){
+    $("panel-heading.att").toggle();
+
+  });
+  $(".panel-heading.extra").click(function(){
+    $(".panel.extra").slideToggle();
+  });
+
+  $(".panel.extra").click(function(){
+    $("panel-heading.extra").toggle();
+
+  });
+  $(".panel-heading.string").click(function(){
+    $(".panel.string").slideToggle();
+  });
+
+  $(".panel.string").click(function(){
+    $("panel-heading.string").toggle();
+
+  });
+
+});
